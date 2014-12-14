af64b3738176dacaf3a7cf5a48cb25700827629b
Sam Selvanathan <santhonyselvanat@paypal.com>
2014-12-13 21:39:08 -0800

    use object proto accessor to call Wreck`s request

--- test/index.js
+++ test/index.js
@@ -4,17 +4,17 @@ var method = 'GET'; // GET, POST, PUT, DELETE
 //var uri = 'https://locahost:8081/';
 var uri =  'https://www.yahoo.com/';
 
-//var req = Wreck.request(method, uri, {}, function (err, res) {
-//    /* handle err if it exists, in which case res will be undefined */
-//
-//    // buffer the response stream
-//    Wreck.read(res, null, function (err, body) {
-//        /* do stuff */
-//        console.log(body.toString());
-//    });
-//});
+var req = Wreck.request(method, uri, {}, function (err, res) {
+    /* handle err if it exists, in which case res will be undefined */
 
-Wreck.get(uri, function (err, res, payload) {
-    console.log(payload.toString());
+    // buffer the response stream
+    Wreck.read(res, null, function (err, body) {
+        /* do stuff */
+        console.log(body.toString());
+    });
 });
+
+//Wreck.get(uri, function (err, res, payload) {
+//    console.log(payload.toString());
+//});
 
