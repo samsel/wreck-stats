287c5e1edf7b8af0f3800ec56d185fe5ef8734b7
Sam Selvanathan <santhonyselvanat@paypal.com>
2014-12-13 19:07:42 -0800

    working version of the timing measure logic for a request

--- readme.md
+++ readme.md
@@ -4,4 +4,8 @@ wreck-stats
 stats plugin for wreck
 
 #### todo
-run tests of wreck from the node_modules folder and make sure it passes.
+* run tests of wreck from the node_modules folder and make sure it passes.
+* logger should be one time configurable or console.log to be used by default
+* wreck-stats should write an object to that as an object with url, method, time, etc.
+* each wreck-stats accessor should be able to pass an id (assigned by default if not) and we should write that id to the logger
+* test with a local host server (that hangs request for a known time) and see if it matches the actual wreck-stats time
--- test/index.js
+++ test/index.js
@@ -1,9 +1,20 @@
-var wreck = require('../lib/index');
+var Wreck = require('../lib/index');
 
-wreck.get('https://www.yahoo.com', {}, function () {
-	//console.dir(arguments);
-});
+var method = 'GET'; // GET, POST, PUT, DELETE
+//var uri = 'https://locahost:8081/';
+var uri =  'https://www.google.com/';
 
-// profiler.get('http://localhost:8080', {}, function () {
-// 	console.dir("arguments");
-// });
+var options = {};
+
+var optionalCallback = function (err, res) {
+
+    /* handle err if it exists, in which case res will be undefined */
+
+    // buffer the response stream
+    Wreck.read(res, null, function (err, body) {
+        /* do stuff */
+        console.log(body.toString());
+    });
+};
+
+var req = Wreck.request(method, uri, options, optionalCallback);
