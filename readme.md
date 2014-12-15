773b5b85b6a8bb58949c03abd566c6542b3dcd36
Sam Selvanathan <santhonyselvanat@paypal.com>
2014-12-14 19:09:44 -0800

    added a configurable logger and exposed the setLogger interface on the Wreck object

--- readme.md
+++ readme.md
@@ -5,6 +5,5 @@ stats plugin for wreck
 
 #### todo
 * run tests of wreck from the node_modules folder and make sure it passes.
-* logger should be one time configurable or console.log to be used by default
 * each wreck-stats accessor should be able to pass an id (assigned by default if not)
   and we should write that id to the logger
--- test/index.js
+++ test/index.js
@@ -1,6 +1,16 @@
-var Wreck = require('../index');
+var Wreck = require('../lib/index');
 //var uri = 'http://0.0.0.0:8080/health';
 var uri =  'http://www.yahoo.com/';
+
+Wreck.setLogger(function (obj) {
+    console.log('setLogger');
+    console.dir(obj);
+});
+
+Wreck.setLogger(function (obj) {
+    console.log('setLogger - I win');
+    console.dir(obj);
+});
 
 Wreck.get(uri, function (err, res, payload) {
     console.log(arguments);
