b48543c0e52f2c7a7139aedcfb8ed9c10b85c330
Sam Selvanathan <sjasel@gmail.com>
2014-12-23 18:03:54 -0800

    added jscs with default airbnb preset

--- test/wreck.js
+++ test/wreck.js
@@ -1,20 +1,21 @@
 'use strict';
 
 /*
-  make sure the Wreck tests pass
-  since wreck-stats is a drop in
-  replacement for Wreck along
-  with some stats logging feature!
-*/
-var Rewire = require('rewire');
-var WreckTests = Rewire('../node_modules/wreck/test/index');
+	make sure the Wreck tests pass
+	since wreck-stats is a drop in
+	replacement for Wreck along
+	with some stats logging feature!
 
-// modify the `Wreck` private variable inside the Wreck's
-// test file with our version of Wreck and our expect library!
-var WreckStats = require('../lib/index');
+	modify the `Wreck` private variable inside the Wreck's
+	test file with our version of Wreck and our expect library!
+*/
+var Rewire = require('rewire'),
+	WreckTests = Rewire('../node_modules/wreck/test/index'),
+	WreckStats = require('../lib/index');
+
 WreckTests.__set__('Wreck', WreckStats);
 WreckTests.__set__('expect', require('code').expect);
 // finally, set a silent logger :)
-WreckStats.setLogger(function(){});
+WreckStats.setLogger(function() {});
 
 exports.lab = WreckTests.lab;
